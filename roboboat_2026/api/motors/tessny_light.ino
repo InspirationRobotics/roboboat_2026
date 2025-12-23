72969772ce7d90274efe2ce6a26b5532932ac49e
Chase <133188311+Chase-520@users.noreply.github.com>
2025-12-22 18:37:21 -0800

    Clean up teensy_light.ino, adding control to water pump at pin 2

--- roboboat_2026/api/motors/tessny_light.ino
+++ roboboat_2026/api/motors/tessny_light.ino
@@ -10,26 +10,24 @@ Servo forward_port;   // ESC1
 Servo forward_starboard; // ESC2
 Servo aft_port;       // ESC3
 Servo aft_starboard;  // ESC4
+Servo pump;
 
 const int escPin1 = 38;
 const int escPin2 = 37;
 const int escPin3 = 36;
 const int escPin4 = 35;
-
+const int pumpPin = 2;
 const int minPulse = 1100;
 const int maxPulse = 1900;
 
 float desire_surge, desire_sway, desire_yaw;
 int pwm1, pwm2, pwm3, pwm4;
-// ======================
-// Variables for GPS
-// ======================
-float raw_lat, raw_lon, raw_heading;
-float raw_speed_knots = 0;
+bool activate_pump = false;
+
 
 float dt = 0.01;  // 100Hz for IMU
 const unsigned long loopInterval = dt * 1000; // milliseconds
-bool debugMode = true;
+
 unsigned long last_gps_time = 0;
 
 // ======================
@@ -37,15 +35,14 @@ unsigned long last_gps_time = 0;
 // ======================
 void setup(){
     Serial.begin(115200);
-    Serial5.begin(115200);
-
 
     forward_port.attach(escPin4);
     forward_starboard.attach(escPin1);
     aft_port.attach(escPin3);
     aft_starboard.attach(escPin2);
+    pump.attach(pumpPin);
 
-    Serial.println("Teensy started with complementary filter");
+    Serial.println("Teensy started Teensy Light");
 }
 
 // ======================
@@ -78,109 +75,31 @@ void moveThrusters(double surge, double sway, double yaw){
 }
 
 // ======================
-// GPS parsing
-// ======================
-double DM_to_DD(String dm) {
-    if (dm.length() < 4) return 0;
-    double v = dm.toFloat();
-    int deg = (int)(v / 100);
-    double minutes = v - deg * 100;
-    return deg + minutes / 60.0;
-}
-
-// ======================
 // Serial parsing
 // ======================
 void parseSerial(String data){
     int c1 = data.indexOf(',');
     int c2 = data.indexOf(',', c1 + 1);
     int c3 = data.indexOf(',', c2 + 1);
+    int c4 = data.indexOf(',', c3 + 1);
 
     desire_surge        = data.substring(0, c1).toFloat();
     desire_sway         = data.substring(c1 + 1, c2).toFloat();
     desire_yaw          = data.substring(c2 + 1, c3).toFloat();
+    int pump_reading        = data.substring(c3 + 1, c4).toFloat();
 
-    if(debugMode){
-    Serial.print("surge: "); Serial.print(desire_surge);
-    Serial.print("lateral: "); Serial.print(desire_sway);
-    Serial.print("yaw: "); Serial.print(desire_yaw);
-    Serial.println("");
+    if(pump_reading==1){
+        Serial.println("Water pump activated");
+        activate_pump = true;
+    }else{
+        activate_pump = false;
     }
-    if(debugMode){
-        Serial.print("PWMs: ");
-        Serial.print(pwm1); Serial.print(" ");
-        Serial.print(pwm2); Serial.print(" ");  
-        Serial.print(pwm3); Serial.print(" ");
-        Serial.print(pwm4); Serial.println();
-    }
+
 }
 
-void parseRMC(String line) {
-    if (!line.startsWith("$GNRMC")) return;
-
-    String field[15];
-    int idx = 0;
-    int start = 0;
-    
-    for (int i = 0; i < line.length() && idx < 15; i++) {
-        if (line[i] == ',' || line[i] == '*') {
-            field[idx++] = line.substring(start, i);
-            start = i + 1;
-        }
-    }
-
-    if (field[2] != "A") return; // No valid fix
-
-    // Latitude
-    if (field[3].length() > 0) {
-        double lat = DM_to_DD(field[3]);
-        if (field[4] == "S") lat = -lat;
-        raw_lat = lat;
-    }
-
-    // Longitude  
-    if (field[5].length() > 0) {
-        double lon = DM_to_DD(field[5]);
-        if (field[6] == "W") lon = -lon;
-        raw_lon = lon;
-    }
-
-    // Speed (knots to m/s)
-    if (field[7].length() > 0) {
-        raw_speed_knots = field[7].toFloat();
-    }
-
-    // Heading
-    if (field[8].length() > 0) {
-        raw_heading = field[8].toFloat();
-    }
-    
-    last_gps_time = millis();
-}
 
 void loop(){
     unsigned long currentTime = millis();
-    
-    // Read GPS
-    while(Serial5.available() > 0){
-        static String line = "";
-        char c = Serial5.read();
-        
-        if(c == '\n'){
-            parseRMC(line);
-            line = "";
-        } else if(c != '\r') {
-            line += c;
-        }
-    }
-
-
-    // Print data
-    // Serial.print(raw_lat, 8);
-    // Serial.print(","); Serial.print(raw_lon, 8);
-    // Serial.print(","); Serial.print(raw_heading, 1);
-    // Serial.print(","); Serial.print(raw_speed_knots);
-    // Serial.println("");
 
     // Parse Serial input
     // Read Serial
@@ -197,6 +116,12 @@ void loop(){
     }
 
     moveThrusters(desire_surge,desire_sway,desire_yaw);
+
+    if(activate_pump){
+        pump.writeMicroseconds(1800);
+    }else{
+        pump.writeMicroseconds(1500);
+    }
 
     unsigned long elapsed = millis() - currentTime;
     if (elapsed < loopInterval) delay(loopInterval - elapsed);
