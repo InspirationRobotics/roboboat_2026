64ec5f29357caf5212c7bd459249fc2fecd2c756
Chase <chace070528@gmail.com>
2025-12-22 19:22:51 -0800

    update usbLink.sh

--- roboboat_2026/usbLink.sh
+++ roboboat_2026/usbLink.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# To read off the information for each device connected by USB to the sub
-
 for sysdevpath in $(find /sys/bus/usb/devices/usb*/ -name dev); do
-    (
-        # Extract the system path(path in /sys)
-        syspath="${sysdevpath%/dev}"
+(
+    syspath="${sysdevpath%/dev}"
+    devname="$(udevadm info -q name -p "$syspath")"
 
-        # Get the device name using udevadm
-        devname="$(udevadm info -q name -p $syspath)"
+    [[ "$devname" == "bus/"* ]] && exit
 
-        # Skip device names starting with "bus/"
-        [[ "$devname" == "bus/"* ]] && exit
+    eval "$(udevadm info -q property --export -p "$syspath")"
 
-        # Export device properties, evaluate them (concatenates arguments into single command, executes in current shell environment)
-        eval "$(udevadm info -q property --export -p $syspath)"
+    [[ -z "$ID_PATH" ]] && exit
+    [[ -z "$ID_SERIAL" ]] && ID_SERIAL="N/A"
 
-        # Skip if the ID path or ID serial number is empty
-        [[ -z "$ID_PATH" ]] && exit
-	    [[ -z "$ID_SERIAL" ]] && exit
-
-        # Print device information
-        echo "/dev/$devname - $ID_PATH - $ID_SERIAL"
-    )
+    echo "/dev/$devname - $ID_PATH - $ID_SERIAL"
+)
 done
