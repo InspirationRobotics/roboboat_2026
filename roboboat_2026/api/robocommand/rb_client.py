cb5b7e7a49046422d316ab92d3018ac488b92785
Chase <chace070528@gmail.com>
2026-01-28 14:17:35 -0800

    RoboCommand Client WIP

---
+++ roboboat_2026/api/robocommand/rb_client.py
@@ -0,0 +1,92 @@
+from datetime import datetime, timezone
+from google.protobuf.timestamp_pb2 import Timestamp
+from roboboat_2026.api.robocommand.msgs.report_pb2 import *
+from roboboat_2026.api.robocommand.report_body import HeartbeatMsg,GatePassMsg
+import socket, struct
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32, Float32MultiArray
+from std_srvs.srv import Trigger
+
+class RBClient(Node):
+    def __init__(self):
+        # General Information
+        self.team_id = "ASTA"
+        self.vehicle_ids = {"Barco_Polo":123,"Crusader":123}
+        
+        # Track basic robot information
+        self.pos = [] # lat,lon
+        self.vel = float() # speed
+        self.current_task = str() # current task
+        
+        # Create Subscribers and Service Servers
+        self.create_subscribers()
+        self.create_services()
+
+        # send loop 
+        self.msg_queue = []
+        self.report_queue = [] 
+        self.organizer = self.create_timer(1, self.organize_loop)  # 1Hz for heatbeat
+        self.reporter  = self.create_timer(0.2, self.report_loop)  # 5Hz for all msgs
+        self.loop_counter = 0
+
+    def __send(self,msg):
+        ts = Timestamp(); ts.FromDatetime(datetime.now(timezone.utc))
+        msg.sent_at.CopyFrom(ts)
+
+        # Serialize (binary protobuf) and send with 4-byte big-endian length prefix,
+        wire = msg.SerializeToString()
+        frame = struct.pack("!I", len(wire)) + wire + "test".encode()
+
+        with socket.create_connection(("10.10.10.1", 50000)) as s:
+            s.sendall(frame)
+
+    def organize_loop(self): # 1 Hz
+        self.report_queue = [HeartbeatMsg(____)]
+        while len(self.report_queue) <= 5 :
+                self.report_queue.append(self.msg_queue.pop(0))
+    
+    def report_loop(self):
+         self.__send(self.report_queue.pop(0))
+    
+    """All subscribers and services"""
+    def create_subscribers(self):
+        self.sub_gps = self.create_subscription(
+            Float32MultiArray,
+            '/GPS',
+            self.gps_callback,
+            3
+        )
+
+        # TODO subscribe to other topics for velocity, robot state, current task
+
+    def create_services(self):
+        # TODO create services that contains str or float array ...etc 
+        self.srv = self.create_service(
+            Trigger,
+            'toggle_water_pump',
+            self.pump_callback
+        )
+
+    """Topic Callbacks"""
+    def gps_callback(msg):
+         pass
+    
+    """Service Callbacks"""
+    
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = RBClient()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        node.get_logger().info("Ctrl-C received")
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ roboboat_2026/api/robocommand/report_body.py
@@ -0,0 +1,69 @@
+# This script contains functions to create report body for robocommand
+from roboboat_2026.api.robocommand.msgs.report_pb2 import *
+
+def HeartbeatMsg(state:str,lat:float,lon:float,speed:float,heading:float,current_task:str):
+    """
+    Args:
+        state (str): [MANUAL,AUTO,KILLED,UNKNOWN]
+        lat (float): Latitude
+        lon (float): Longitude
+        speed (float): speed in m/s
+        heading (float): heading
+        current_task(str): [UNKNOWN,NONE,NAV_CHANNEL,SPEED_CHALLENGE,OBJECT_DELIVERY,DOCKING,SOUND_SIGNAL]
+
+    Returns:
+        Heartbeat message
+    """
+    states = ["UNKNOWN","KILLED","MANUAL","AUTO"]
+    report_states = [RobotState.STATE_UNKOWN,
+                     RobotState.STATE_KILLED,
+                     RobotState.STATE_MANUAL,
+                     RobotState.STATE_AUTO
+                     ]
+
+    tasks =  ['UNKNOWN','NONE','NAV_CHANNEL','SPEED_CHALLENGE','OBJECT_DELIVERY','DOCKING','SOUND_SIGNAL']
+    report_tasks = [TaskType.TASK_UNKNOWN,
+                    TaskType.TASK_NONE,
+                    TaskType.TASK_ENTRY_EXIT,
+                    TaskType.TASK_NAV_CHANNEL,
+                    TaskType.TASK_SPEED_CHANLLENGE,
+                    TaskType.TASK_OBJECT_DELIVERY,
+                    TaskType.TASK_DOCKING,
+                    TaskType.TASK_SOUND_SIGNAL
+                    ]
+
+    if state not in states or current_task not in tasks:
+        return None
+    
+    return Heartbeat(
+        state = report_states[states.index(state)]
+        position = Latlng(latitude=lat, longitude=lon)
+        spd_mps = float(speed)
+        heading_deg = float(heading)
+        current_task = report_tasks[tasks.index(current_task)]
+    )
+
+def GatePassMsg(type, lat, lon):
+    """
+    Args:
+        Type (String): ENTRY or EXIT
+        lat (float): Latitude
+        lon (float): Longitude
+
+    Returns:
+        GatePass message
+    """
+    if type == "ENTRY":    
+        return GatePass(
+            type=GateType.GATE_ENTRY,
+            position=LatLng(latitude=lat, longitude=lon),
+        )
+
+    if type == "EXIT":    
+        return GatePass(
+            type=GateType.GATE_EXIT,
+            position=LatLng(latitude=lat, longitude=lon),
+        )
+
+    return None
+
