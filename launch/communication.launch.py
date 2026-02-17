838cd605c3fe39b15895875bd77f9ae4c3db504e
Chase <chace070528@gmail.com>
2026-02-17 04:24:56 -0800

    update communication launch

--- launch/communication.launch.py
+++ launch/communication.launch.py
@@ -3,6 +3,7 @@ from launch.actions import IncludeLaunchDescription, ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
+from launch.actions import TimerAction
 import os
 
 
@@ -10,14 +11,25 @@ def generate_launch_description():
 
 
     # gps node
-    gps_node = Node(
+    ivc_node = Node(
         package='roboboat_2026',
-        executable='gps_node',
+        executable='ivc_node',
+        output='screen'
+    )
+
+    report_node = Node(
+        package='roboboat_2026',
+        executable='report_node',
         output='screen'
     )
     
 
 
     return LaunchDescription([
+        ivc_node,
 
+        TimerAction(
+            period=2.0,
+            actions=[report_node]
+        ),
     ])
