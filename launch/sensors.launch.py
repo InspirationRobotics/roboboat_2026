f85da4c5d71b4dc2dcec92f72b6c2e080d487af2
Chase <chace070528@gmail.com>
2026-02-17 04:14:16 -0800

    add timer action for nodes

--- launch/sensors.launch.py
+++ launch/sensors.launch.py
@@ -1,34 +1,27 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
+from launch.actions import TimerAction
 from launch_ros.actions import Node
-import os
 
 
 def generate_launch_description():
-    # gps node
     gps_node = Node(
         package='roboboat_2026',
         executable='gps_node',
         output='screen'
     )
 
-    # Teensy node
     teensy_node = Node(
         package='roboboat_2026',
         executable='teensy_node',
         output='screen'
     )
 
-    # launcher node
     launcher_node = Node(
         package='roboboat_2026',
         executable='launcher_node',
         output='screen'
     )
 
-    # led node
     led_node = Node(
         package='roboboat_2026',
         executable='led_node',
@@ -37,7 +30,19 @@ def generate_launch_description():
 
     return LaunchDescription([
         gps_node,
-        teensy_node,
-        launcher_node,
-        led_node
+
+        TimerAction(
+            period=2.0,
+            actions=[teensy_node]
+        ),
+
+        TimerAction(
+            period=4.0,
+            actions=[launcher_node]
+        ),
+
+        TimerAction(
+            period=6.0,
+            actions=[led_node]
+        ),
     ])
